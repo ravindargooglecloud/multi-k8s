fcc66aa9ed86c8a7c6e962471e79c191dd25bd38
ravindartravis <ravindranath.rajaganapathy@gmail.com>
2022-08-01 09:32:00 +0530

    testing commit

--- server/index.js
+++ server/index.js
@@ -35,23 +35,25 @@ const redisClient = redis.createClient({
 const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
-
 app.get("/", (req, res) => {
   res.send("Hi");
 });
 
+// retrieve all values that had been submitted from 'postgres' table
 app.get("/values/all", async (req, res) => {
   const values = await pgClient.query("SELECT * from values");
 
   res.send(values.rows);
 });
 
+// retrieve all the requested indices and calculated value for each index from 'Redis'
 app.get("/values/current", async (req, res) => {
   redisClient.hgetall("values", (err, values) => {
     res.send(values);
   });
 });
 
+// route handler to handle new value being submitted
 app.post("/values", async (req, res) => {
   const index = req.body.index;
 
